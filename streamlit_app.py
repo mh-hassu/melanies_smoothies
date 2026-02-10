7b5db92c817f65a29e7e3d1b32eb66b56d276f00
Muhammad Hassaan <136970814+mh-hassu@users.noreply.github.com>
2026-02-10 09:16:29 +0500

    Fetch nutrition info for selected fruits dynamically

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen + ' Nutrition Information ')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     #st.write(ingredients_string)
 
@@ -47,7 +48,8 @@ if ingredients_list:
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-        
+
+      
         st.success('Your Smoothie is ordered!', icon="✅")
 # New section to display smoothiefroot nutrition information
 
