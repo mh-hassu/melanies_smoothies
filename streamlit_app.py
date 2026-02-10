ff78bd40fad8ce6a093aff6bc3bfdbee2abaabc6
Muhammad Hassaan <136970814+mh-hassu@users.noreply.github.com>
2026-02-10 08:20:53 +0500

    Fix session reference for fetching fruit options

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ name_on_order = st.text_input('Name on Smoothiee')
 st.write('The name on your smoothie will be: ', name_on_order)
 
 #session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = Session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect (
